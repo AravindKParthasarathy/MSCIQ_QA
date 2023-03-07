bafaa4547fe2b0068486f21b3b511b4f6fa2ec0c
AravindKumarParthasarathy <aravindkparthasarathy@gmail.com>
2023-03-08 00:42:21 +0530

    Test

---
+++ MSCIQ_QA/src/main/java/base/Base.java
@@ -0,0 +1,8 @@
+package base;
+
+public class Base {
+	
+	public static void main() {
+	System.out.println("Hello");
+
+}}
---
+++ MSCIQ_QA/src/test/java/com/msciq/MSCIQ_QA/AppTest.java
@@ -0,0 +1,20 @@
+package com.msciq.MSCIQ_QA;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
